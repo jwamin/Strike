1e45a0e5791e25d60ff9a3426cd7a537e883731f
Joss Manger <jossferatu@gmail.com>
2020-08-02 11:09:07 -0400

    Initial Commit

---
+++ Strike/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Strike
+//
+//  Created by Joss Manger on 8/2/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Strike/StrikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  StrikeApp.swift
+//  Strike
+//
+//  Created by Joss Manger on 8/2/20.
+//
+
+import SwiftUI
+
+@main
+struct StrikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
